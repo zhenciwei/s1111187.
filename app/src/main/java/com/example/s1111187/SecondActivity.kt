67ffd317befbfd6a3327e8b1453552a16e99fd16
student <janice900123@gmail.com>
2024-05-22 16:35:22 +0800

    V3_王英禎

--- app/src/main/java/com/example/s1111187/SecondActivity.kt
+++ app/src/main/java/com/example/s1111187/SecondActivity.kt
@@ -1,7 +1,6 @@
 package com.example.s1111187
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,11 +8,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,55 +18,50 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.s1111187.ui.theme.S1111187Theme
 
-@OptIn(ExperimentalMaterial3Api::class)
-
-class MainActivity : ComponentActivity() {
+class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             S1111187Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     //Greeting("Android")
                     //Main()
-                    FirstScreen()
-
+                    SecondScreen()
                 }
             }
         }
     }
 }
-
 @Composable
-fun FirstScreen() {
+fun SecondScreen() {
     val context = LocalContext.current
+    val activity = (context as Activity)
+    Column {
 
-    Column(modifier = Modifier) {
-        Image(
-            painter = painterResource(id = R.drawable.maria),
-            contentDescription = "maria"
-        )
+        Image(painter = painterResource(id = R.drawable.maria),
+            contentDescription = "maria")
 
         Text(
-            text = "簡介",
-            color = Color.Blue
+            text = "主要機構",
+            color = Color.Red
         )
-        
-        Image(painter = painterResource(id = R.drawable.service),
-            contentDescription = "service")
 
-        Button(onClick = {
-            var it = Intent(context, SecondActivity::class.java)
-            context.startActivity(it)
-        })
+        Image(painter = painterResource(id = R.drawable.aurthor), contentDescription = "aurthor")
+
+        Button(
+            onClick = {
+                activity.finish()
+            })
         {
-            Text(text = "資管系王英禎")
+            Text(text = "服務總覽")
         }
 
     }
 }
+
